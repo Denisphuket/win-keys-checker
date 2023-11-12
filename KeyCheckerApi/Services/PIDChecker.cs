ee928784580282fa4ac67b040eb61915b13619e1
Denisphuket <taxiphuket@yandex.ru>
2023-11-12 10:32:31 +0700

    first PID

--- KeyCheckerApi/Services/PIDChecker.cs
+++ KeyCheckerApi/Services/PIDChecker.cs
@@ -93,6 +93,8 @@ namespace KeyCheckerApi.Services
                 var RetID = -1;
                 string pKeyConfig = "";
 
+                Console.WriteLine($"pkeyConfigList: {pkeyConfigList}");
+
                 foreach (var config in pkeyConfigList)
                 {
                     pKeyConfig = config;
